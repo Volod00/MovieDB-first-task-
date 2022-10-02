57655d811b5ea396be542fd7cd0ea7c36db49300
Volodymyr Dev <evroprostir@gmail.com>
2022-10-02 15:24:51 +0300

    first commit

---
+++ javascript/fileOne.js
@@ -0,0 +1,10 @@
+// alert('Hello');
+// const result = confirm("Are you here?");
+// console.log(result);
+
+const answers = [];
+answers[0] = prompt("What is your name?"," ");
+answers[1] = prompt("What is your surname?"," ");
+answers[2] = prompt("How old are you?"," ");
+
+document.write(answers);
