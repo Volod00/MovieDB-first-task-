8d5acc0d1cdaa209d9a43ae875decefa91a76531
Yatsiv Volodymyr <volodymyr.yatsiv@mfa.gov.ua>
2022-10-05 17:51:27 +0300

    add functions

--- javascript/mainPage.js
+++ javascript/mainPage.js
@@ -1,33 +1,53 @@
-const numberOfFilms = +prompt("How many movies have you already watched?","");
+let numberOfFilms;
+
+function start() {
+        numberOfFilms = +prompt("How many movies have you already watched?","");
+
+        while(numberOfFilms == '' || numberOfFilms == null|| isNaN(numberOfFilms)) {
+                numberOfFilms = +prompt("How many movies have you already watched?",""); 
+        }
+}
+
+start();
 
 const personalMovieDB = {
         count:numberOfFilms,
-        movie:{},
+        movies:{},
         actors:{},
         genres:[],
         private:false
 };
-console.log(personalMovieDB)
 
-for (let i = 0; i < 2; i++){
-        let  movieName = prompt("which movie did you watch last?",""),
-             rank = +prompt("Please rank it",'');
-     if (movieName.length < 50 && movieName != "" && rank !="" && movieName != null && rank != null){
-                        personalMovieDB.movie[movieName] = rank;
-                        console.log('done');
+
+function rememberMyFilms(){
+        for (let i = 0; i < 2; i++){
+                const  movieName = prompt("which movie did you watch last?",""),
+                       rank = +prompt("Please rank it",'');
+             
+                       if (movieName.length < 50 && movieName != "" && rank !="" && movieName != null && rank != null){
+                                personalMovieDB.movies[movieName] = rank;
+                                console.log('done');
+                } else {
+                        i--;
+                        console.log('error');
+                }
+           };
+}
+
+rememberMyFilms();
+
+function detectLevel(){
+                if (personalMovieDB.count < 10){
+                console.log("You wathced not many movies");
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count <=30){
+                console.log("You are classic cinema viewer");
+        } else if (personalMovieDB.count >= 30){
+                console.log("You are movie fan");
         } else {
-                i--;
-                console.log('error');
-        }
-   };
- 
-if (personalMovieDB.count < 10){
-        console.log("You wathced not many movies");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count <=30){
-        console.log("You are classic cinema viewer");
-} else if (personalMovieDB.count >= 30){
-        console.log("You are movie fan");
-} else {
-        console.log("Error");
-};
+                console.log("Error");
+        };
+  }
+detectLevel(); 
+
+
 
